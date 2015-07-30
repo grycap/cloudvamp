b8c97ba37b051251472127721d9d571117273fd0
dealfonso <caralla@upv.es>
2015-07-30 09:44:18 +0200

    bug in setup.py

--- cvem/setup.py
+++ cvem/setup.py
@@ -15,7 +15,7 @@
 # limitations under the License.                                             #
 #--------------------------------------------------------------------------- #
 
-from setuptools import setup
+from distutils.core import setup
 import os
 
 # Add special files
